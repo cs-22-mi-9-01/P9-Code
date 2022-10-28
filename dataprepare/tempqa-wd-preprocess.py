08775f2b2f62eacefc92fdcb4141292604d7aeeb
Ning An <1196244890@qq.com>
2022-10-28 10:48:29 +0200

    use numpy

--- dataprepare/tempqa-wd-preprocess.py
+++ dataprepare/tempqa-wd-preprocess.py
@@ -1,18 +1,28 @@
+from calendar import c
 import re
 from collections import Counter
+import numpy as np
 
-jsquestion = []
-que_pron = []
+
 def json_to_triple():
+    question = np.array([])
+    que_pron = np.array([])
     with open('tempqa-wd/data/dev.json') as f:
+        # find the question in json
         for line in f.readlines():
             line = re.findall('"TEXT": ".*?"', line)
             if line != []:
-                jsquestion.append(line)
-        for i in range(len(jsquestion)):
-            que_pron.append([jsquestion[i][0].split(" ")[1].strip('"')].pop(0))
-        que_counter = dict(Counter(que_pron))
-        print(f'Interrogative Pronouns:', que_counter)
+                question = np.append(question, line)
+
+        for i in range(len(question)):
+            # extract question as a nparray
+            question[i] = question[i].split('"')[3]
+            # count the nubmer of IPron
+            que_pron = np.append(que_pron, [question[i].split( ).pop(0)])
+        que_pron = np.array(que_pron)
+        unique, unique_counts = np.unique(que_pron, return_counts=True)
+        print(unique, unique_counts)
+        
 
 if __name__ == '__main__':
     json_to_triple()
