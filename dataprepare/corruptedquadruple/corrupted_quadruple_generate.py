5595e7c39ddd52629288f4136ccf36a8e6f6711a
Ning An <1196244890@qq.com>
2022-11-02 13:14:24 +0100

    generate corrupted quadruple in json format

---
+++ dataprepare/corruptedquadruple/corrupted_quadruple_generate.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import csv
+import re
+import json
+
+# convert txt into csv
+def txt_to_csv(filename):
+    with open(f"{filename.split('/')[-1].split('.')[0]}.csv", 'w') as f1:
+        writer = csv.writer(f1)
+        csv_row = []
+        writer.writerow(csv_row)
+        with open(filename) as f2:
+            for line in f2:
+                line = re.split("['\t','\n']", line)
+                writer.writerow(line)
+
+# generate corrupted quadruple and save it as csv file
+def generate_corrupted_quadruple(filename):
+    with open(filename) as f:
+        with open('corrupted_quad.csv', 'a') as f2:
+            for line in f:
+                list = line.split(',')[0:-1]
+                a = np.array(list)
+                co_qu = np.array([])
+                for i in range(4):
+                    if i == 0:
+                        co_qu = np.array([0, a[1], a[2], a[3], a[0]])
+                        f2.write(','.join(co_qu) + '\n')
+                    elif i == 1:
+                        co_qu = np.array([a[0], 0, a[2], a[3], a[1]])
+                        f2.write(','.join(co_qu) + '\n')
+                    elif i == 2:
+                        co_qu = np.array([a[0], a[1], 0, a[3], a[2]])
+                        f2.write(','.join(co_qu) + '\n')
+                    elif i == 3:
+                        co_qu = np.array([a[0], a[1], a[2], 0, a[3]])
+                        f2.write(','.join(co_qu) + '\n')
+
+# convert csv into json
+def csv_to_json(filename):
+    with open('corrupted_quad.json', 'w') as f:
+        all_data = pd.read_csv(filename)
+        for i in range(len(all_data)):
+            row = all_data.loc[i,:].tolist()
+            json.dump(row, f, indent=4)
+
+    
+
+if __name__ == '__main__':
+    # txt_to_csv('icews05-15/test.txt')
+    # generate_corrupted_quadruple('test.csv')
+    csv_to_json('corrupted_quad.csv')
