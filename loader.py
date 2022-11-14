fcb5a2757d7802b66b2ffb06f3dec95f3dc3b3ca
Jeppe Lindberg <jepp3441@gmail.com>
2022-11-14 09:35:53 +0100

    First run DE_SimplE, DE_TransE, DE_DistMult

--- loader.py
+++ loader.py
@@ -15,6 +15,7 @@ class Loader:
             for key in dict.keys():
                 if ' ' in key:
                     target_key = key
+                    break
             if target_key is not None:
                 dict[target_key.replace(' ', ' ')] = dict.pop(target_key)
             else:
