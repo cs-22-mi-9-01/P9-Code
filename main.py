ff097390995d8ece78bcd709dfe0764e94aba830
Jones <jlindb18@student.aau.dk>
2022-11-28 09:51:39 +0100

    fix

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-dataset', type=str, default='icews14', choices=['icews14', 'icews05-15', 'gdelt'])
-    parser.add_argument('-embedding', type=str, default='TERO', choices=['all', 'DE_TransE', 'DE_SimplE', 'DE_DistMult', 'TERO', 'ATISE'])
+    parser.add_argument('-embedding', type=str, default='all', choices=['all', 'DE_TransE', 'DE_SimplE', 'DE_DistMult', 'TERO', 'ATISE'])
     parser.add_argument('-add_to_result', type=bool, default=True)
 
     args = parser.parse_args()
